9e4c78cdd76cda51be8a8377e848340f1ee7591d
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-04-05 20:54:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener('keydown', (e) => {
+
+    // Allow only numbers and control keys
+    if (e.key >= 0 && e.key <= 9) {
+      code.value = "";
+      setTimeout(() => {
+        if (idx < codes.length - 1) {
+          codes[idx + 1].focus();
+        }
+      }, 10);
+    }
+
+    // Handle backspace
+    else if (e.key === "Backspace") {
+      setTimeout(() => {
+        if (code.value === "" && idx > 0) {
+          codes[idx - 1].focus();
+        }
+      }, 10);
+    }
+
+  });
+});
